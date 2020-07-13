3bbf05c0194f997ed3fe4a93e544e2ab47827105
Pavel G <grdv.pvl@gmail.com>
2020-07-14 02:04:26 +0300

    first iteration of simple gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+// import { version } from '../package.json';
+import genDiff from '../src/index.js';
+
 program
   .name('gendiff')
   .version('0.0.1')
@@ -8,6 +11,9 @@ program
   .usage('[options] <filepath1> <filepath2>')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
-  .arguments('[type]');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-export default genDiff = (filepath1, filepath2) => {
-    return diff;
+import fs from 'fs';
+import _ from 'lodash';
+
+const readFile = (pathToFile) => JSON.parse(fs.readFileSync(pathToFile, 'utf-8'));
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const dataBefore = readFile(pathToFile1);
+  const dataAfter = readFile(pathToFile2);
+  const allKeys = _.uniq(Object.keys(dataBefore), Object.keys(dataAfter));
+  console.log(allKeys);
+  const test = allKeys.map((key) => {
+    if (!_.has(dataAfter, key)) {
+      return [`  - ${key}: ${dataBefore[key]}`];
+    }
+    if (!_.has(dataBefore, key)) {
+      return [`  + ${key}: ${dataAfter[key]}`];
+    }
+    if (dataBefore[key] !== dataAfter[key]) {
+      return [`  - ${key}: ${dataBefore[key]}\n  + ${key}: ${dataAfter[key]}`];
+    }
+    return [`    ${key}: ${dataBefore[key]}`];
+  });
+  const test2 = test.join('\n');
+  return `{\n${test2}\n}`;
 };
+export default genDiff;
