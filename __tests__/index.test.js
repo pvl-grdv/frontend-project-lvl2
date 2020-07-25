83e050475b99647cb86a5f164ee3cc307ba95422
Pavel G <grdv.pvl@gmail.com>
2020-07-26 00:13:42 +0300

    fix __filename global const

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import genDiff from '../src';
 
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join('__tests__/__fixtures__/', `${filename}`);
