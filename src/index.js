053ef9171472ef585861722dd2f64e3b5db36170
Pavel G <grdv.pvl@gmail.com>
2020-08-25 03:50:28 +0300

    fix ERR_UNSUPPORTED_DIR_IMPORT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import parser from './parsers.js';
 import { readFile, getType } from './utils.js';
-import render from './formatters';
+import render from './formatters/index.js';
 import astBuilder from './astBuilder.js';
 
 export default (firstConfig, secondConfig, format) => {
