7c61688bba36ee70d5ae525ad902f417eef3ddd0
Pavel G <grdv.pvl@gmail.com>
2020-09-19 02:25:51 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,20 @@
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+import path from 'path';
 import genDiff from '../src';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const getPath = (filename) => path.join('__fixtures__', filename);
 
-const getPath = (filename) => path.join(__dirname, '__fixtures__', filename);
-
-const fileTypes = ['json', 'yml', 'ini'];
+const fileFormats = ['json', 'yml', 'ini'];
 const outputTypes = ['nested', 'plain', 'json'];
 
 const testArguments = outputTypes.flatMap((format) => (
-  fileTypes.map((filetype) => [filetype, format])
+  fileFormats.map((filetype) => [filetype, format])
 ));
 
-test.each(testArguments)('type: %s, output format: %s', (type, format) => {
-  const before = getPath(`nestedBefore.${type}`);
-  const after = getPath(`nestedAfter.${type}`);
-  const actual = genDiff(before, after, format);
-  const expected = fs.readFileSync(getPath(`${format}Result`), 'utf-8');
+test.each(testArguments)('File format: %s, output type: %s', (fileFormat, outputType) => {
+  const before = getPath(`before.${fileFormat}`);
+  const after = getPath(`after.${fileFormat}`);
+  const actual = genDiff(before, after, outputType);
+  const expected = fs.readFileSync(getPath(`${outputType}Result`), 'utf-8');
   expect(actual).toBe(expected);
 });
