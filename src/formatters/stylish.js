5a488b84c525b93f818b901068fe40be6bdf8ef0
Pavel G <grdv.pvl@gmail.com>
2020-09-30 17:56:31 +0300

    refactor generation of indentation and Newline

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,37 +1,36 @@
 import _ from 'lodash';
 
-const initialIndent = 2;
-const extraIndent = 4;
-const generateIndent = (depth) => ' '.repeat(initialIndent + depth * extraIndent);
-
+const generateIndent = (depth) => '  '.repeat(depth);
 const stringify = (item, depth) => {
   if (!_.isObject(item)) {
     return item;
   }
   const result = Object.entries(item)
-    .map(([key, value]) => `${generateIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
+    .map(([key, value]) => `${generateIndent(depth + 2)}  ${key}: ${stringify(value, depth + 2)}`);
   return ['{', ...result, `${generateIndent(depth)}  }`].join('\n');
 };
 
-const render = (diff, depth = 0) => diff.map((node) => {
-  const {
-    type, name: key, value, valueBefore, valueAfter, children,
-  } = node;
+const render = (diff, depth = 1) => {
+  const result = diff.flatMap((node) => {
+    const {
+      type, name: key, value, valueBefore, valueAfter, children,
+    } = node;
 
-  switch (type) {
-    case 'unchanged':
-      return `${generateIndent(depth)}  ${key}: ${stringify(value, depth)}`;
-    case 'added':
-      return `${generateIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
-    case 'deleted':
-      return `${generateIndent(depth)}- ${key}: ${stringify(value, depth)}`;
-    case 'changed':
-      return `${generateIndent(depth)}- ${key}: ${stringify(valueBefore, depth)}\n${generateIndent(depth)}+ ${key}: ${stringify(valueAfter, depth)}`;
-    case 'nested':
-      return `${generateIndent(depth)}  ${key}: {\n${render(children, depth + 1)}\n${generateIndent(depth)}  }`;
-    default:
-      throw new Error(`Unknown node type: '${type}'`);
-  }
-}).join('\n');
-
-export default (diff) => `{\n${render(diff)}\n}`;
+    switch (type) {
+      case 'unchanged':
+        return `${generateIndent(depth)}  ${key}: ${stringify(value, depth)}`;
+      case 'added':
+        return `${generateIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
+      case 'deleted':
+        return `${generateIndent(depth)}- ${key}: ${stringify(value, depth)}`;
+      case 'changed':
+        return [`${generateIndent(depth)}- ${key}: ${stringify(valueBefore, depth)}`, `${generateIndent(depth)}+ ${key}: ${stringify(valueAfter, depth)}`];
+      case 'nested':
+        return `${generateIndent(depth)}  ${key}: ${render(children, depth + 2)}`;
+      default:
+        throw new Error(`Unknown node type: '${type}'`);
+    }
+  });
+  return `{\n${result.join('\n')}\n${generateIndent(depth - 1)}}`;
+};
+export default render;
